4e7db4c1729997bb8b5767ddeab8d29382328892
ljyockey <linyockey@gmail.com>
2017-03-21 15:53:14 -0700

    added new classes to HTML, added pseudo code to js

---
+++ app.js
@@ -0,0 +1,16 @@
+/*function that reveals first question of quiz after
+clicking "begin"*/
+
+/*function that checks answers after clicking "check
+answer". Add green font to correct and red to user's
+answer if it wasn't correct*/
+
+/*function that adds the amount of right answers*/
+
+/*function that hides current question and reveals 
+next question after clicking "next"*/
+
+/*function to add the amount correct to "results"*/
+
+/*function that refreshes page at end to start 
+again*/
